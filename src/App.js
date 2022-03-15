7c17b47c84307a9efa970afa2951b41c26108180
horsemaker <yash_ghodekar@yahoo.in>
2022-03-16 00:48:36 +0530

    feat: basic structure with routes & navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Header from "./frontend/components/Navbar/Navbar";
+import ResponsiveNavbar from "./frontend/components/ResponsiveNavbar/ResponsiveNavbar";
+import HomeScreen from "./frontend/screens/HomeScreen/HomeScreen";
+import AboutUsScreen from "./frontend/screens/AboutUsScreen/AboutUsScreen";
+import ProductsScreen from "./frontend/screens/ProductsScreen/ProductsScreen";
+import CartScreen from "./frontend/screens/CartScreen/CartScreen";
+import WishlistScreen from "./frontend/screens/WishlistScreen/WishlistScreen";
+import SignInScreen from "./frontend/screens/SignInScreen/SignInScreen";
+import SignUpScreen from "./frontend/screens/SignUpScreen/SignUpScreen";
+import { useNavbar } from "./frontend/contexts/navbar-context";
+
 function App() {
-  return <div className="App"></div>;
+  const { showNavbar } = useNavbar();
+  const appClassName = `app${showNavbar ? " overflowY-hidden" : ""}`;
+
+  return (
+    <div className={appClassName}>
+      <Header />
+      <ResponsiveNavbar />
+      <Routes>
+        <Route path="/" element={<HomeScreen />} />
+        <Route path="/aboutus" element={<AboutUsScreen />} />
+        <Route path="/products" element={<ProductsScreen />} />
+        <Route path="/cart" element={<CartScreen />} />
+        <Route path="/wishlist" element={<WishlistScreen />} />
+        <Route path="/signin" element={<SignInScreen />} />
+        <Route path="/signup" element={<SignUpScreen />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
