39f1604f665e763f58b54212230f7a369b4fcbef
horsemaker <yash_ghodekar@yahoo.in>
2022-03-16 23:50:58 +0530

    feat: add mockman

--- src/frontend/routes/Router.js
+++ src/frontend/routes/Router.js
@@ -9,18 +9,22 @@ import {
   SignUpScreen,
   WishlistScreen,
 } from "../screens";
+import Mockman from "mockman-js";
 
 const Router = () => {
   return (
-    <Routes>
-      <Route path="/" element={<HomeScreen />} />
-      <Route path="/about" element={<AboutUsScreen />} />
-      <Route path="/products" element={<ProductsScreen />} />
-      <Route path="/cart" element={<CartScreen />} />
-      <Route path="/wishlist" element={<WishlistScreen />} />
-      <Route path="/signin" element={<SignInScreen />} />
-      <Route path="/signup" element={<SignUpScreen />} />
-    </Routes>
+    <main className="main-page">
+      <Routes>
+        <Route path="/" element={<HomeScreen />} />
+        <Route path="/about" element={<AboutUsScreen />} />
+        <Route path="/products" element={<ProductsScreen />} />
+        <Route path="/cart" element={<CartScreen />} />
+        <Route path="/wishlist" element={<WishlistScreen />} />
+        <Route path="/signin" element={<SignInScreen />} />
+        <Route path="/signup" element={<SignUpScreen />} />
+        <Route path="/mockman" element={<Mockman />} />
+      </Routes>
+    </main>
   );
 };
 
