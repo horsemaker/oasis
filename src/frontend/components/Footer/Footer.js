598c0f23053336a2694ab5b64cf6fd258ded3316
horsemaker <yash_ghodekar@yahoo.in>
2022-03-16 17:26:14 +0530

    fix: `classNameName` to `className`

--- src/frontend/components/Footer/Footer.js
+++ src/frontend/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import "./Footer.css";
 
 const Footer = () => {
   return (
-    <footer classNameName="footer">
+    <footer className="footer">
       <div className="footer-brand">
         <Link to="/">
           <img
