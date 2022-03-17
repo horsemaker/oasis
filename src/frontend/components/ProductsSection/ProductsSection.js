7d1062dd371d9e274090f7e33590722180c669e4
horsemaker <yash_ghodekar@yahoo.in>
2022-03-17 22:46:43 +0530

    add Products Section

---
+++ src/frontend/components/ProductsSection/ProductsSection.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { ProductCard } from "../ProductCard/ProductCard";
+import "./ProductsSection.css";
+
+const ProductsSection = ({ children, products }) => {
+  return (
+    <div className="products-section">
+      <h1 className="h1-60">{children}</h1>
+      <div className="products">
+        {products.map(
+          ({
+            _id,
+            name,
+            originalPrice,
+            discount,
+            discountedPrice,
+            rating,
+            images,
+            isBestseller,
+            isTrending,
+          }) => (
+            <ProductCard
+              key={_id}
+              _id={_id}
+              name={name}
+              originalPrice={originalPrice}
+              discount={discount}
+              discountedPrice={discountedPrice}
+              rating={rating}
+              images={images}
+              isBestSeller={isBestseller}
+              isTrending={isTrending}
+            />
+          )
+        )}
+      </div>
+    </div>
+  );
+};
+
+export { ProductsSection };
