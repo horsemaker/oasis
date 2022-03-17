3e3a0a52d5d732a81fdc5b2b76bb2ae62e349150
horsemaker <yash_ghodekar@yahoo.in>
2022-03-17 22:47:56 +0530

    barrel export ProductsSection, ProductCard, Rating

--- src/frontend/components/index.js
+++ src/frontend/components/index.js
@@ -2,3 +2,6 @@ export { Navbar } from "./Navbar/Navbar";
 export { ResponsiveNavbar } from "./ResponsiveNavbar/ResponsiveNavbar";
 export { Footer } from "./Footer/Footer";
 export { Banner } from "./Banner/Banner";
+export { ProductsSection } from "./ProductsSection/ProductsSection";
+export { ProductCard } from "./ProductCard/ProductCard";
+export { Rating } from "./Rating/Rating";
