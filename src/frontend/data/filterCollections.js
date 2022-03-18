2ddf1c3ca8b5313fa43f7d22e84902d91dc83b2d
horsemaker <yash_ghodekar@yahoo.in>
2022-03-18 19:40:47 +0530

    add filter data

---
+++ src/frontend/data/filterCollections.js
@@ -0,0 +1,50 @@
+export const filterCollections = [
+  {
+    id: 1,
+    heading: "size",
+    filters: [
+      { filterName: "small", filterIcon: "/assets/Oasis/Icons/shop_small.svg" },
+      {
+        filterName: "medium",
+        filterIcon: "/assets/Oasis/Icons/shop_medium.svg",
+      },
+      {
+        filterName: "large",
+        filterIcon: "/assets/Oasis/Icons/shop_large.svg",
+      },
+    ],
+  },
+  {
+    id: 2,
+    heading: "sunlight",
+    filters: [
+      { filterName: "low", filterIcon: "/assets/Oasis/Icons/sunlight_low.svg" },
+      {
+        filterName: "medium",
+        filterIcon: "/assets/Oasis/Icons/sunlight_medium.svg",
+      },
+      {
+        filterName: "high",
+        filterIcon: "/assets/Oasis/Icons/sunlight_high.svg",
+      },
+    ],
+  },
+  {
+    id: 3,
+    heading: "maintenance",
+    filters: [
+      {
+        filterName: "low",
+        filterIcon: "/assets/Oasis/Icons/maintenance_low.svg",
+      },
+      {
+        filterName: "medium",
+        filterIcon: "/assets/Oasis/Icons/maintenance_medium.svg",
+      },
+      {
+        filterName: "high",
+        filterIcon: "/assets/Oasis/Icons/maintenance_high.svg",
+      },
+    ],
+  },
+];
