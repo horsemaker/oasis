c9884f63fdd0ba128deb6100408aea08aec4ab22
horsemaker <yash_ghodekar@yahoo.in>
2022-03-22 01:37:04 +0530

    add auth provider

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import {
+  AuthProvider,
   CategoriesProvider,
   FiltersProvider,
   NavbarProvider,
   ProductsProvider,
 } from "./frontend/contexts";
+// import Mockman from "mockman-js";
 import { makeServer } from "./server";
 
 // Call make Server
@@ -16,15 +18,17 @@ makeServer();
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <NavbarProvider>
-        <ProductsProvider>
-          <CategoriesProvider>
-            <FiltersProvider>
-              <App />
-            </FiltersProvider>
-          </CategoriesProvider>
-        </ProductsProvider>
-      </NavbarProvider>
+      <AuthProvider>
+        <NavbarProvider>
+          <ProductsProvider>
+            <CategoriesProvider>
+              <FiltersProvider>
+                <App />
+              </FiltersProvider>
+            </CategoriesProvider>
+          </ProductsProvider>
+        </NavbarProvider>
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
