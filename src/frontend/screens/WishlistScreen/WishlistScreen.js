fd003f2013013a92187501f3ae16ef9d3d10af00
horsemaker <yash_ghodekar@yahoo.in>
2022-03-22 19:22:22 +0530

    add Wishlist Screen

--- src/frontend/screens/WishlistScreen/WishlistScreen.js
+++ src/frontend/screens/WishlistScreen/WishlistScreen.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { WishlistProductCard } from "../../components";
+import { useWishlist } from "../../contexts";
+import "./WishlistScreen.css";
 
 function WishlistScreen() {
-  return <div>WishlistScreen</div>;
+  const { wishlist } = useWishlist();
+
+  return (
+    <div className="wishlist-screen">
+      {wishlist.map((wishlistProduct) => (
+        <WishlistProductCard
+          key={wishlistProduct._id}
+          product={wishlistProduct}
+        />
+      ))}
+    </div>
+  );
 }
 
 export { WishlistScreen };
