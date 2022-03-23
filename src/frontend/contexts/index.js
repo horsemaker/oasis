1cccd1254b674109c73c4fc72d4f7b09d9098ebc
horsemaker <yash_ghodekar@yahoo.in>
2022-03-24 00:12:49 +0530

    add cart context

--- src/frontend/contexts/index.js
+++ src/frontend/contexts/index.js
@@ -4,3 +4,4 @@ export { CategoriesProvider, useCategories } from "./categories-context";
 export { FiltersProvider, useFilters } from "./filters-context";
 export { AuthProvider, useAuth } from "./auth-context";
 export { WishlistProvider, useWishlist } from "./wishlist-context";
+export { CartProvider, useCart } from "./cart-context";
